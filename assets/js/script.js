08e7212a44866dcb4acab453b29f9a3991e03772
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-07-30 08:31:40 -0500

    Updated filter criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,13 +96,14 @@ const filterFunc = function (selectedValue) {
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
+const betterFilter = document.querySelectorAll("[data-filter-item]");
 
 for (let i = 0; i < filterBtn.length; i++) {
 
   filterBtn[i].addEventListener("click", function () {
 
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
+    let selectedValue = betterFilter.innerText.toLowerCase();
+    selectValue.innerText = betterFilter.innerText;
     filterFunc(selectedValue);
 
     lastClickedBtn.classList.remove("active");
